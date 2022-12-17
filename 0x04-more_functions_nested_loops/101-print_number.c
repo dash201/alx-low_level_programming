8e3e74f1b22ae7d01dbf9c0e3cd0a379a2c466e5
dash201 <liamidisad24@gmail.com>
2022-12-16 18:15:23 -0800

    Write a function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,17 +1,29 @@
 #include "main.h"
+
+/**
+ *len - return the length of number
+ *@len: parameter int
+ *Return: a Value
+*/
+int len(int len)
+{
+	int value;
+
+	value = 1;
+	while (len / 10 != 0)
+	{
+		len = len / 10;
+		value = value * 10;
+	}
+	return (value);
+}
 /**
  *print_number - print a number
  *@n: parameter type int
 */
 void print_number(int n)
 {
-	int value = 1;
-
-	while (n / 10 != 0)
-	{
-		n = n / 10;
-		value = value * 10;
-	}
+	int value = len(n);
 
 	while (value != 0)
 	{
