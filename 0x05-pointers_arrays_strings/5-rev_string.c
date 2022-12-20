04f38ea75024751acf9698a2e8356a90063a9760
dash201 <liamidisad24@gmail.com>
2022-12-19 16:32:16 -0800

    Write a function that reverses a string.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,12 +6,13 @@
 void rev_string(char *s)
 {
 	int i = _strlen(&(*s)) - 1;
-	char c;
+	char c[i + 2];
 
 	while (i >= 0)
 	{
 		c[i] = s[i];
 		i = i - 1;
 	}
+	c[_strlen(&(*s))] = '\0';
 	*s = c;
 }
