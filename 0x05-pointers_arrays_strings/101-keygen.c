dbac313b3cafc2242359615f4ba90a02c2cb691f
dash201 <liamidisad24@gmail.com>
2022-12-20 18:50:19 -0800

    Create a program that generates random valid passwords for the program 101-crackme.

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,48 +6,6 @@
 #include <unistd.h>
 #include <time.h>
 
-void print_array(int *a, int n)
-{
-    int i = 0;
-    for (i = 0; i < (n - 1); i++)
-        printf("%d, ", a[i]);
-    printf("%d\n", a[i]);
-}
-
-char *_strcpy(char *dest, char const *src)
-{
-    int a = 0;
-    int x = 0;
-
-    for(a = 0; src[a] != '\0'; a++) {
-        dest[x] = src[a];
-        x++;
-    }
-    dest[x] = '\0';
-    return (dest);
-}
-
-int _atoi(char *str)
-{
-
-    int a = 0, nb = 0, i = 1, n = 0;
-
-    for (a = 0; str[a] != '\0' && str[a] < '0' || str[a] > '9'; a++) {
-        (str[a] == '-') ? i = i * -1 : 0;
-    }
-    while (str[a] >= '0' && str[a] <= '9') {
-        n = (n * 10) + (str[a] - 48);
-        if (n == 2147483647 && i == -1)
-            return (-2147483647);
-        if (n < 0 || n > 2147483647)
-            return 0;
-        else
-            nb = n;
-        a++;
-    }
-    return(nb * i);
-}
-
 char *create_password(void)
 {
     int i = 0, b = 0, a = 127;
@@ -64,7 +22,7 @@ char *create_password(void)
     return(pasw);
 }
 
-int main(int ac, char **av)
+int main(void)
 {
     char dest[10];
     int sp[] = {1, 4, 6, 3, 7, 9, 0, 13, 56, 23, 78};
