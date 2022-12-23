9cdcefde63cee7d1e7a36f38df03a728ddb4ac26
dash201 <liamidisad24@gmail.com>
2022-12-22 16:22:02 -0800

    Write a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -25,7 +25,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int len = _strlen(&(*dest));
 
-	for (i = 0; i < len && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[len + i] = src[i];
 	dest[len + i] = '\0';
 	return (dest);
