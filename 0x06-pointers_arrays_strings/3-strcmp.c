362f1bbe98f0bf0030c0decfe4f7cd87b1250b4e
dash201 <liamidisad24@gmail.com>
2022-12-22 17:19:37 -0800

    Write a function that compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ *_strlen -  returns the length of a string
+ *@s: parameter
+ *Return: length of a string
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ *equal_case - equality of words
+ *@s1: first parameter
+ *@s2: second parameter
+ *Return: number
+*/
+int equal_case(char *s1, char *s2)
+{
+	for (int i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] > s2[i])
+			return (15);
+		else if (s1[i] < s2[i])
+			return (-15);
+	}
+	return (0);
+}
+/**
+ *_strcmp - compares two string
+ *@s1: first parameter
+ *@s2: second parameter
+ *Return: char
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int n = 0;
+	int n1 = _strlen(s1);
+	int n2 = _strlen(s2);
+
+	if (n1 > n2)
+		return (15);
+	else if (n1 < n2)
+		return (-15);
+	else
+	{
+		equal_case(s1, s2);
+	}
+}
