3e637cb73aaadce5087dfbf68c64167fb3c79ad2
dash201 <liamidisad24@gmail.com>
2022-12-22 16:36:42 -0800

    Update header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 void _putchar(char c);
