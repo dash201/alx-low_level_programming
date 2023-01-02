b234b5c394c5a52d88635125cd0a280718a46f0d
dash201 <liamidisad24@gmail.com>
2023-01-02 08:30:01 -0800

    Write a function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ *_strlen -  returns the length of a string
+ *@s: parameter
+ *Return: length of a string
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+/**
+ *_memset -this function fills memory with a contant byte
+ *@s: first parameter
+ *@b: second parameter
+ *@n: third parameter
+ *Return: char pointer
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int len = _strlen(s);
+	int result = len - n;
+	char *cp = *s;
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	for (; i < len; i++)
+	{
+		s[i] = *cp;
+	}
+	return s;
+}
