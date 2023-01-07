d0436fcc0dd709ab5ad081e78b38e10f7075b9ac
dash201 <liamidisad24@gmail.com>
2023-01-06 16:29:09 -0800

    Write a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-#ifdef UNUSED
-#elif defined(__GNUC__)
-# define UNUSED(x) UNUSED_ ## x __attribute__((unused))
-#elif defined(__LCLINT__)
-# define UNUSED(x) /*@unused@*/ x
-#else
-# define UNUSED(x) x
-#endif
 /**
  *main - entry program
  *@argc: first parameter
  *@argv: second parameter
  *Return: always 0
 */
-int main(int UNUSED(argc), char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
