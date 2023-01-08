9b56e73bd8b6bf6cd8e30142242d0e06f5c5a820
dash201 <liamidisad24@gmail.com>
2023-01-08 15:01:05 -0800

    Write a program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ *ismember -  veriify the string
+ *@str: parammeter
+ *Return: 0 or 1
+*/
+int isnumber (char * str)
+{
+	int a = 0;
+	if (str[0] == '-' || str[0] == '+')
+		a = 1;
+	for (a = 0; str[a] != '\0'; a++) {
+		if (str[a] < '0' || str[a] > '9')
+		return 1;
+	}
+	return 0;
+}
+/**
+ *main - entrey program
+ *@argc: first parameter
+ *@argv: second parameter
+ *Return:0 or 1
+*/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int nb = 0; int a;
+
+	if (argc == 1)
+                printf("0\n");
+	else
+	{
+		for (a = 1; argv[a] != NULL; a++)
+		{
+			if (isnumber (argv[a]) == 1)
+			{
+				printf ("Error\n");
+				return 1;
+			}
+			nb = nb + atoi (argv[a]);
+		}
+		printf ("%d\n", nb);
+	}
+	return 0;
+}
