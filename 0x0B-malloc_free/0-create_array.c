0e8a763d30b86468c90508332ab796b20ca99620
dash201 <liamidisad24@gmail.com>
2023-01-10 05:28:12 -0800

    Write a function that creates an array of chars, and initializes it with a specific char.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stddef.h>
 /**
  *create_array -  creates an array of chars, and initializes
  *it with a specific char.
@@ -12,8 +13,15 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	s = malloc(sizeof(char) * size);
-	for (i = 0; i < size; i++)
-		s[i] = c;
-	return (s);
+	if (s != NULL)
+	{
+		for (i = 0; i < size; i++)
+			s[i] = c;
+		return (s);
+	}
+	else
+		return (NULL);
 }
