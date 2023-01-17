e493c8cd8bb1630aef36163c561ad9d523515d89
dash201 <liamidisad24@gmail.com>
2023-01-16 16:16:50 -0800

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ *struct dog - dog structures
+ *@name: first parameter
+ *@owner: second parameter
+ *@age: thirst parameter
+ *
+ *Example of structure represented by a dog
+*/
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
