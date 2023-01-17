b03d3af5730b683ff69b99bf7eb38d5e2b6e04f8
dash201 <liamidisad24@gmail.com>
2023-01-16 17:25:07 -0800

    Define a new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,16 +4,16 @@
 /**
  *struct dog - dog structures
  *@name: first parameter
- *@owner: second parameter
- *@age: thirst parameter
+ *@age: second parameter
+ *@owner: thirst parameter
  *
  *Example of structure represented by a dog
 */
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
 
 /**
