303ec0ae4466f8ee77adeaec7c85822d37d1284f
dash201 <liamidisad24@gmail.com>
2023-01-19 10:58:02 -0800

    This file should contain the function that selects the correct function to perform the operation asked by the user.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ *op_add - addition of two numbers
+ *@a: first number
+ *@b: second number
+ *Return: sum of a and b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - substraction of two numbers
+ *@a: first parameter
+ *@b: second parameter
+ *Return: difference of a and b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - multiply two number
+ *@a: first parameter
+ *@b: second parameter
+ *Return: the product of a and b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - divide two number
+ *@a: first parameter
+ *@b: second parameter
+ *Return: the result of the division of a by b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - modulo of two numbers
+ *@a: first parameter
+ *@b: second parameter
+ *Return: the remainder of the division of a by b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
