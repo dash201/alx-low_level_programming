6b9352e6eccd1bf41894f79c6ac43a67be88adc2
dash201 <liamidisad24@gmail.com>
2023-01-18 18:30:38 -0800

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,5 @@
 #include "function_pointers.h"
 #include <stddef.h>
-
-/**
- *print - function used to print
- *@c: parameter
-*/
-void print(char *c)
-{
-	int i;
-
-	for (i = 0; c[i] != '\0'; i++)
-		_putchar(c[i]);
-}
 /**
  *print_name - print a name
  *@name: name
