e3c0f9eda62ea332fdda4431cf1ccb0ace5f6997
dash201 <liamidisad24@gmail.com>
2023-01-19 16:48:42 -0800

    contain the function that selects the correct function to perform the operation asked by the user

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,10 +19,10 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while ((ops[i].op) != NULL)
 	{
-		if (ops[i].op == s)
-			return (*(ops[i].f));
+		if ((ops[i].op) == s)
+			return (ops[i].f);
 		i++;
 	}
 	return (NULL);
