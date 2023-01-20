cebc78ec15f2bbb9053c175c37015970d19b331c
dash201 <liamidisad24@gmail.com>
2023-01-19 16:21:40 -0800

    contain the main function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,7 @@
 #include "3-calc.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  *main - entry of program
  *@argc: first parameter
@@ -9,7 +11,23 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	if (argc != 4)
-		return (1);
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (strcmp(argv[2], "+") == 0 || strcmp(argv[2], "*") == 0 ||
+	    strcmp(argv[2], "-") == 0 || strcmp(argv[2], "/") ==0 ||
+	    strcmp(argv[2], "%") == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) &&
+	    atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 	return (0);
 }
