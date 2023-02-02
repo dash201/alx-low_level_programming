b427bd693f424b6b75548d9eca48dda7421c1850
dash201 <liamidisad24@gmail.com>
2023-02-02 09:26:00 -0800

    0. 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stddef.h>
-#include <stdlib.h>
 /**
  *_strlen -  returns the length of a string
  *@s: parameter
@@ -31,7 +30,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = len; i >= 0; i--)
 	{
-		if (b[i] <0 || b[i] > 1)
+		if (b[i] < 0 || b[i] > 1)
 			return (0);
 		if (b[i] == 1)
 			total += decval;
