324533ff85135f28b5dcdebd47cbfa6ef8335bec
dash201 <liamidisad24@gmail.com>
2023-02-02 18:57:03 -0800

    2. 10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,6 +9,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
 	for (i = 0; i < index; i++)
 		;
 	return ((n >> i) & 1);
